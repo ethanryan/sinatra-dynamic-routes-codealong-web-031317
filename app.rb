3d00bf2b822b07295cc56097b1d46dde3040b163
ethanryan <ethanryan@gmail.com>
2017-04-04 11:14:58 -0400

    done

--- app.rb
+++ app.rb
@@ -13,5 +13,18 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  # goodbye name
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+  end
 
-end
+  # multiply nums
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1] #these are strings!
+    @num2 = params[:num2] #these are strings!
+    @result = @num1.to_i * @num2.to_i #need to turn strings into integers, then multiply
+    "#{@result}"
+  end
+
+end #end class
